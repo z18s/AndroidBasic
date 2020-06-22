de728590324c22ec3df717c223320a6cde78930d
Konstantin <57114608+z18s@users.noreply.github.com>
2020-06-22 15:01:37 +0300

    Homework Three

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,5 +24,48 @@ public class MainActivity extends AppCompatActivity {
 
         TextView dateToday = findViewById(R.id.dateToday);
         dateToday.setText(new SimpleDateFormat("dd/MM").format(dateNow));
+
+        Toast.makeText(getApplicationContext(), "Activity created", Toast.LENGTH_SHORT).show();
+        Log.d("INFO", "Activity created");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Toast.makeText(getApplicationContext(), "Activity started", Toast.LENGTH_SHORT).show();
+        Log.d("INFO", "Activity started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Toast.makeText(getApplicationContext(), "Activity stopped", Toast.LENGTH_SHORT).show();
+        Log.d("INFO", "Activity stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(getApplicationContext(), "Activity destroyed", Toast.LENGTH_SHORT).show();
+        Log.d("INFO", "Activity destroyed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Toast.makeText(getApplicationContext(), "Activity paused", Toast.LENGTH_SHORT).show();
+        Log.d("INFO", "Activity paused");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(getApplicationContext(), "Activity resumed", Toast.LENGTH_SHORT).show();
+        Log.d("INFO", "Activity resumed");
     }
 }
