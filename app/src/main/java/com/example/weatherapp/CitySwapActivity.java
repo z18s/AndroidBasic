48a931cc17f9612b03151fe0ce7ccc25bdbcabfe
Konstantin <57114608+z18s@users.noreply.github.com>
2020-06-25 22:59:56 +0300

    Homework Four

--- app/src/main/java/com/example/weatherapp/CitySwapActivity.java
+++ app/src/main/java/com/example/weatherapp/CitySwapActivity.java
@@ -1,9 +1,9 @@
 package com.example.weatherapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 
@@ -13,12 +13,44 @@ public class CitySwapActivity extends Activity {
 
     ActivityCitiesBinding binding;
 
+    private String currentCity = null;
+    private String chosenCity = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityCitiesBinding.inflate(LayoutInflater.from(this));
         setContentView(binding.getRoot());
 
-        ListView citiesList = findViewById(R.id.citiesList);
+        currentCity = getIntent().getStringExtra(Constants.CURRENT_CITY);
+        binding.textCitySelect.setText(currentCity);
+
+        selectCityByButton();
+        selectCityByList();
     }
+
+    private void selectCityByButton() {
+        binding.buttonSwapCity.setOnClickListener((view) -> {
+            chosenCity = binding.textCitySelect.getText().toString();
+            returnToMainActivity();
+        });
+    }
+
+    private void selectCityByList() {
+        binding.citiesList.setOnItemClickListener((adapterView, view, i, l) -> {
+            chosenCity = binding.citiesList.getItemAtPosition(i).toString();
+            returnToMainActivity();
+        });
+    }
+
+    private void returnToMainActivity() {
+        if (chosenCity != null && !chosenCity.equals("")) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra(Constants.CHOSEN_CITY, chosenCity);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
+
+
 }
