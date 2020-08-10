6c1f9f7c10dafdf7a58f4b36570d2372218472d1
Konstantin <57114608+z18s@users.noreply.github.com>
2020-08-11 02:38:02 +0300

    Homework Eight finished

---
+++ app/src/main/java/com/example/weatherapp/model/Wind.java
@@ -0,0 +1,30 @@
+package com.example.weatherapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Wind {
+
+    @SerializedName("speed")
+    @Expose
+    private Double speed;
+    @SerializedName("deg")
+    @Expose
+    private Integer deg;
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public Integer getDeg() {
+        return deg;
+    }
+
+    public void setDeg(Integer deg) {
+        this.deg = deg;
+    }
+}
